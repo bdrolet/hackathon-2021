f1b10d9e4cb3c5d9cd78c26d2830fc854a5e2acd
Navya Kalale <nkalale@lyft.com>
2021-09-09 12:11:22 -0400

    call handler from api

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,8 +1,11 @@
 from typing import Dict
+from typing import List
+from typing import Any
 from flask import blueprints
 from marshmallow import fields
 from marshmallow import validate
 from webargs.flaskparser import use_kwargs
+from app.handlers.decomposition_handler import calculate_estimate
 
 
 blueprint = blueprints.Blueprint("hackathon-2021", __name__)
@@ -17,3 +20,8 @@ def hello_world(*, name: str) -> Dict[str, str]:
     # your data transformation logic should go in here.
     # Business logic should be made into new modules and called from the endpoint.
     return {"msg": f"Hello, {name}!"}
+
+
+def get_response(tasks: List[Dict[str, Any]]):
+    # call calculate_estimate function in decomposition_handler
+    return calculate_estimate(tasks)
